3f812b86f3e484da26fe5bbb5ad7da455011caf9
siva4717 <sivagopi@4717>
2025-09-24 15:04:43 +0530

    Roboshop project using aws cli

--- 01-roboshop.sh
+++ 01-roboshop.sh
@@ -28,7 +28,7 @@ do
             ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{
-                "Value"         : "'" $DOMIAN_NAME "'"
+                "Value"         : "'" $IP "'"
             }]
         }
         }]
