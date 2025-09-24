1f415c076db22fdb0fb281abdc80e84ae52c1a07
siva4717 <sivagopi@4717>
2025-09-24 14:53:43 +0530

    Roboshop project using aws cli

--- 01-roboshop.sh
+++ 01-roboshop.sh
@@ -1,16 +1,24 @@
 #?/bin/bash
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0ad4f00017b011267"
-
+HOSTED_ZONE="Z043346914G8XVEOOX7JH"
+DOMIAN_NAME="msgd.fun"
 
 for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME=$instance.$DOMIAN_NAME #mongodb.msgd.fun
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME=$DOMIAN_NAME #msgd.fun
     fi
     echo "$instance:$IP"
 
-done
+    aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE 
+    --change-batch '{ "Comment": "Updating record", 
+    "Changes": [ { "Action": "UPSERT", "ResourceRecordSet": { "Name": 
+    "$instance", "Type": "A", "TTL": 1, "ResourceRecords": [ { "Value": "$IP" } ] } } ] }'
+
+done    
