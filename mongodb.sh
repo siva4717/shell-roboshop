faf5dedb86ce8e6b693132a136c45a5ecea3dc27
siva4717 <sivagopi@4717>
2025-09-24 16:22:13 +0530

    Roboshop project using aws cli

--- mongodb.sh
+++ mongodb.sh
@@ -24,11 +24,11 @@ VALIDATE(){
     fi
 }
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-VALIDATE $? "Adding mongo repo" &>>$FILE_LOG
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$FILE_LOG
+VALIDATE $? "Adding mongo repo"
 dnf install mongodb-org -y &>>$FILE_LOG
-VALIDATE $? "mongodb" &>>$FILE_LOG
-systemctl enable mongod
-VALIDATE $? "Systemctl enable" &>>$FILE_LOG 
-systemctl start mongod 
-VALIDATE $? "Systemctl start" &>>$FILE_LOG
+VALIDATE $? "mongodb" 
+systemctl enable mongod &>>$FILE_LOG
+VALIDATE $? "Systemctl enable"  
+systemctl start mongod &>>$FILE_LOG
+VALIDATE $? "Systemctl start" 
