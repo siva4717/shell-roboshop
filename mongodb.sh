6ce979b50973eb9e4eb01945682d4bd990c033c9
siva4717 <sivagopi@4717>
2025-09-24 16:14:00 +0530

    Roboshop project using aws cli

--- mongodb.sh
+++ mongodb.sh
@@ -6,6 +6,7 @@ Y="\e[33m"
 N="\e[0m"
 FILE_LOG_DIRECTORY="/var/log/shell-roboshop/"
 SCRIPT_NAME=$(echo $0 | cut -d '.' -f1)
+SCRIPT_DIRECTORY=$pwd
 FILE_LOG=$FILE_LOG_DIRECTORY/$SCRIPT_NAME.log
 mkdir -p $FILE_LOG_DIRECTORY 
 echo -e "$G The script Started at ::: $(date)$N"
@@ -24,7 +25,7 @@ VALIDATE(){
     fi
 }
 
-cp mongo.repo vim /etc/yum.repos.d/mongo.repo
+cp $SCRIPT_DIRECTORY/mongo.repo vim /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Adding mongo repo" &>>$FILE_LOG
 dnf install mongodb-org -y &>>$FILE_LOG
 VALIDATE $? "mongodb" &>>$FILE_LOG
