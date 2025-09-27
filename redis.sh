b5b21fa49f24863950323e36d0a7e35d9551ac40
siva4717 <sivagopi@4717>
2025-09-27 14:43:03 +0530

    message

--- redis.sh
+++ redis.sh
@@ -33,6 +33,9 @@ VALIDATE $? "Disable Redis"
 dnf module enable redis:7 -y &>>$FILE_LOG
 VALIDATE $? "Enable Redis"
 
+dnf install redis -y  &>>$FILE_LOG
+VALIDATE $? "Enable Redis"
+
 sed -i -e 's/127.0.0.1/0.0.0.0/' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>>$FILE_LOG  
 VALIDATE $? "Allowing remote connections and protected-mode no"
 
