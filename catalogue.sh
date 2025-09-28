375cdd05d25b5cb9a11c96bd2ab09f3cdc8d7757
siva4717 <sivagopi@4717>
2025-09-28 07:48:45 +0530

    shell-roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -1,88 +1,88 @@
-#?/bin/bash
-USER_ID=$(id -u)
+#!/bin/bash
+
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-FILE_LOG_DIRECTORY="/var/log/shell-roboshop/"
-SCRIPT_NAME=$(echo $0 | cut -d '.' -f1)
-SCRIPT_DIRECTORY=$PWD
-FILE_LOG=$FILE_LOG_DIRECTORY/$SCRIPT_NAME.log
-MONGODB_SERVER="mongodb.msgd.fun"
-mkdir -p $FILE_LOG_DIRECTORY 
-echo -e "$G The script Started at ::: $(date)$N"
 
-if [ $USER_ID -ne 0 ]; then 
-    echo -e " $R You can use root user $N" 
-    exit 1  
-    
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.msgd.fun
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
 fi
 
-VALIDATE(){
+VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo -e " $2 ... $R failure $N "
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
     else
-        echo -e " $2 ... $G success $N "
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
-dnf module disable nodejs -y &>>$FILE_LOG
-VALIDATE $? "Disable nodejs" &>>$FILE_LOG
+##### NodeJS ####
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disabling NodeJS"
+dnf module enable nodejs:20 -y  &>>$LOG_FILE
+VALIDATE $? "Enabling NodeJS 20"
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing NodeJS"
 
-dnf module enable nodejs:20 -y &>>$FILE_LOG
-VALIDATE $? "Enable nodejs:20"
-
-dnf install nodejs -y &>>$FILE_LOG
-VALIDATE $? "Installing nodejs"
-
-id roboshop &>> $FILE_LOG
+id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]; then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$FILE_LOG
-    VALIDATE $? "create system user"
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating system user"
 else
-    echo -e " $Y System user is already created $N"
+    echo -e "User already exist ... $Y SKIPPING $N"
 fi
 
-mkdir -p /app  &>>$FILE_LOG
-VALIDATE $? "create directory"
+mkdir -p /app
+VALIDATE $? "Creating app directory"
 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$FILE_LOG
-cd /app  &>>$FILE_LOG
-
-rm -rf /app/*
-VALIDATE $? "removing existing code"
-
-unzip /tmp/catalogue.zip  &>>$FILE_LOG
-VALIDATE $? "unzip"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading catalogue application"
 
 cd /app 
-cp $SCRIPT_DIRECTORY/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Changing to app directory"
 
-npm install &>>$FILE_LOG
-VALIDATE $? "npm install" 
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
 
-systemctl daemon-reload &>>$FILE_LOG
-VALIDATE $? "Daemon reload"
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+VALIDATE $? "unzip catalogue"
 
-systemctl enable catalogue &>>$FILE_LOG
-VALIDATE $? "enable catalogue" 
+npm install &>>$LOG_FILE
+VALIDATE $? "Install dependencies"
 
-systemctl start catalogue &>>$FILE_LOG
-VALIDATE $? "start catalogue"
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Copy systemctl service"
 
-cp $SCRIPT_DIRECTORY/mongo.repo /etc/yum.repos.d/mongo.repo &>>$FILE_LOG
-VALIDATE $? "adding mongo repo"
+systemctl daemon-reload
+systemctl enable catalogue &>>$LOG_FILE
+VALIDATE $? "Enable catalogue"
 
-dnf install mongodb-mongosh -y &>>$FILE_LOG
-VALIDATE $? "Installing mongosh"
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copy mongo repo"
 
-INDEX=$(mongosh mongodb.msgd.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "Install MongoDB client"
+
+INDEX=$(mongosh mongodb.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
 if [ $INDEX -le 0 ]; then
-    mongosh --host $MONGODB_SERVER </app/db/master-data.js &>>$FILE_LOG
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Load catalogue products"
 else
     echo -e "Catalogue products already loaded ... $Y SKIPPING $N"
 fi
 
-systemctl restart catalogue  &>>$FILE_LOG  
-VALIDATE $? "systemctl restart"
+systemctl restart catalogue
+VALIDATE $? "Restarted catalogue"
